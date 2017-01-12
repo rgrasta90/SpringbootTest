8c3f223d01b556b12bc0fa2f10680f305b3833f1
rgrasta90 <rodolfo_jr_g@hotmail.com>
2017-01-11 23:19:18 -0600

    Adding AddToCart

--- src/main/java/com/webstore/controller/AppController.java
+++ src/main/java/com/webstore/controller/AppController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.webstore.model.Product;
@@ -28,4 +29,11 @@ public class AppController {
 		System.out.println("In get all products controller");
 		return service.getAllProducts();
 	}
+	
+	@RequestMapping("/addtocart/{id}/{user}")
+	public @ResponseBody String addToCart(@RequestParam Long id, @RequestParam String user){
+		
+		return null;
+	}
+
 }
