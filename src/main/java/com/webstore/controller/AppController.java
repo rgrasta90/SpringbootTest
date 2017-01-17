9a54ace14967ce159ac2f1dfbff1e29616e41de4
rgrasta90 <rodolfo_jr_g@hotmail.com>
2017-01-16 23:28:50 -0600

    Adding Order functionality

--- src/main/java/com/webstore/controller/AppController.java
+++ src/main/java/com/webstore/controller/AppController.java
@@ -2,6 +2,8 @@ package com.webstore.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +11,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.webstore.AppRunner;
+import com.webstore.model.CartItem;
+import com.webstore.model.OrderDetails;
 import com.webstore.model.Product;
 import com.webstore.service.CartService;
+import com.webstore.service.OrderService;
 import com.webstore.service.PackageService;
 
 
 @Controller
 public class AppController {
 
+	private static final Logger log = LoggerFactory.getLogger(AppRunner.class);
 	@Autowired
 	PackageService service;
 	@Autowired 
-	CartService cartservice;
+	CartService cartservice;	
+	@Autowired
+	OrderService orderservice;
 	
 	@RequestMapping("/")
 	public String welcome(){
@@ -37,8 +46,11 @@ public class AppController {
 	@RequestMapping(value = "/addtocart", method = RequestMethod.POST)
 	public @ResponseBody String addToCart(@RequestParam(name="gameid", required=false) String gameid, @RequestParam(name="username", required=false ) String username){
 		System.out.println("In add to cart controller" + " " + username + " " + gameid);
-		cartservice.addToCart(gameid, username);
+		CartItem c = cartservice.addToCart(gameid, username);
+		System.out.println("Succesfully added" + " " + c.getName() + "to cart" );
+		OrderDetails o = orderservice.saveOrder(c.getName(), username);
+		log.info("Succesfully created order" + " " + o.getId());
 		return null;
 	}
-
+	
 }
