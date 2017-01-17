020ece45b7469662025fd0b954c763f173ae27d8
rgrasta90 <rodolfo_jr_g@hotmail.com>
2017-01-16 23:29:19 -0600

    Adding Order functionality

--- src/main/java/com/webstore/model/CartItem.java
+++ src/main/java/com/webstore/model/CartItem.java
@@ -21,6 +21,38 @@ protected CartItem(){
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
 	private String name;
 	private int price;
 	private String user;
